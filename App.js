719cb1d8ea86b1c5a7ffb4b03edda1bcec7c1acd
DanielMendesdoAmaral <daniel.amaral720@gmail.com>
2021-04-28 10:32:17 -0300

    Arrumar package

--- App.js
+++ App.js
@@ -2,92 +2,93 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
-import { HubConnectionBuilder } from '@microsoft/signalr';
+import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
 
 import * as Location from 'expo-location';
 
+const hubEndpoint = "http://192.168.1.103:5000/hubs/users";
+
 export default function App() {
-  const [latitude, setLatitude] = useState(0);
-  const [longitude, setLongitude] = useState(0);
-  const [connection, setConnection] = useState(null);
+    const [location, setLocation] = useState(0);
 
-  useEffect(() => {
-    const newConnection = new HubConnectionBuilder()
-      .withUrl('http://192.168.1.103:5000/hubs/users')
-      .withAutomaticReconnect()
-      .build();
+    const [connection, setConnection] = useState(null);
 
-    setConnection(newConnection);
-  }, []);
+    useEffect(() => {
+        const newConnection = new HubConnectionBuilder()
+            .withUrl(hubEndpoint)
+            .withAutomaticReconnect()
+            .configureLogging(LogLevel.Debug)
+            .build();
 
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        console.log("Acess denied!");
-        return;
-      }
+        setConnection(newConnection);
+    }, []);
 
-      let location = await Location.watchPositionAsync({
-        accuracy: Location.Accuracy.High,
-        timeInterval: 10000,
-        distanceInterval: 1
-      },
-        (location) => {
-          setLatitude(location.coords?.latitude);
-          setLongitude(location.coords?.longitude);
-          mudarLocalizacao();
-        })
-    })();
-  }, []);
+    useEffect(() => {
+      (async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          console.log("Acess denied!");
+          return;
+        }
+    
+        let location = await Location.watchPositionAsync({
+          accuracy: Location.Accuracy.High,
+          timeInterval: 1000,
+          distanceInterval: 1
+        },
+          l => {
+            setLocation(l.coords);
+            mudarLocalizacao();
+          })
+      })();
+    }, []);
 
-  const mudarLocalizacao = async () => {
-    let command = {
-      id: "d7f9ce6b-dea6-4aa8-8d3f-2f6145ff2ec9",
-      latitude: latitude,
-      longitude: longitude
-    };
+    const mudarLocalizacao = async () => {
+        let command = {
+            id: "d7f9ce6b-dea6-4aa8-8d3f-2f6145ff2ec9",
+            latitude: location.latitude,
+            longitude: location.longitude
+        };
 
-    if (connection.connectionStarted) {
-      try {
-        await connection.send('MudarLocalizacao', command);
-      }
-      catch (e) {
-        console.log(e);
-      }
+        connection.start();
+
+        connection
+            .invoke('MudarLocalizacao', command)
+            .catch(err => {
+                console.log(err);
+            });
     }
-    else {
-      console.log('No connection to server yet.');
-    }
-  }
 
-  useEffect(() => {
-    if (connection) {
-      connection.start()
-        .then(result => {
-          console.log('Connected!');
-          connection.on('ReceberLocalizacao', dados => {
-            console.log(dados)
-          });
-        })
-        .catch(e => console.log('Connection failed: ', e));
-    }
-  }, [connection]);
+    return (
+        <View style={styles.container}>
+            <Text>Latitude: {location.latitude}</Text>
+            <Text>Longitude: {location.longitude}</Text>
+            <TouchableOpacity onPress={() => {
+                let command = {
+                    id: "d7f9ce6b-dea6-4aa8-8d3f-2f6145ff2ec9",
+                    qtd: 1
+                };
 
-  return (
-    <View style={styles.container}>
-      <Text>Latitude: {latitude}</Text>
-      <Text>Longitude: {longitude}</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+                connection.start();
+
+                connection
+                    .invoke("AlterarEntregas", command)
+                    .catch(err => {
+                        console.log(err);
+                    });
+            }}>
+                <Text>Entregar 1 pacote</Text>
+            </TouchableOpacity>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
 });
